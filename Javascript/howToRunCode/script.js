f5cfbb8e159f8598ad7d5e8132fa2e9f7477a9fa
prashant106yadav <prashant106yadav@gmail.com>
2026-04-10 11:46:59 +0530

    experiments

--- Javascript/howToRunCode/script.js
+++ Javascript/howToRunCode/script.js
@@ -1,3 +1,6 @@
-alert("yokoso watasaio eren yeager")
-document.body.style.backgroundColor = "lightblue";
+// alert("yokoso watasaio eren yeager")
+// document.body.style.backgroundColor = "lightblue";
 console.log("prashant yadav");
+
+let intro = `hello ${name}, my age is ${age} and I play vollyball.`
+console.log(intro);
