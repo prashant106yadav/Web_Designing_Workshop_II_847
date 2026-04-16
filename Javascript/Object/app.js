21dde186ab9a8b6a9d63f64b5e16f88706045870
prashant106yadav <prashant106yadav@gmail.com>
2026-04-16 14:46:02 +0530

    arrays and objects

---
+++ Javascript/Object/app.js
@@ -0,0 +1,23 @@
+let person = {
+    name : "prashant" ,
+    age : 20,
+    gender : "male" 
+};
+
+// console.log(typeof(person))
+
+let array = [1,2,3,4,5];
+
+// console.log(array[4]);
+// console.log(typeof(array));
+// console.log(array.length);
+
+// function prasahnt () {
+//     console.log("prashant yadav");
+    
+// }
+// prasahnt();
+
+function 
+
+
